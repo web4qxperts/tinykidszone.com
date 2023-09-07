6a4d6c41155a9772d61085dbbf8310b621b52591
web4qxperts <72157967+web4qxperts@users.noreply.github.com>
2023-09-07 21:58:10 +0530

    added document

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html lang="en">
+
+    <Head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+<link rel="manifest" href="/manifest.json" />
+<link href="/css/swiper.min.css" rel="stylesheet" />
+<link href="/css/main.css" rel="stylesheet" />
+<script src="/js/jquery.min.js"></script>
+<script src="/js/jquery-ui.min.js"></script>    
+<script src="/js/jquery-ui.min-punch.js"></script>
+<script src="/js/swiper.min.js"></script>
+<script src="/js/main.js"></script>
+</Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
 export default function Home() {
-    return <h1>Hello, Next.js!</h1>
+    return <div>
+        <title>Kids Learning Games</title>
+        </div>
 }
