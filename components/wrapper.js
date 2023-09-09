a3855ab9137c2fbaafaa06929b32d73d038b8b06
web4qxperts <72157967+web4qxperts@users.noreply.github.com>
2023-09-09 13:56:30 +0530

    added

--- components/wrapper.js
+++ components/wrapper.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Wrapper(props) {
     return <div id="root">
@@ -6,7 +7,7 @@ export default function Wrapper(props) {
         <link href="/css/main.css" rel="stylesheet" />
         </Head>
     <header>
-    <div className="logo animate__animated animate__swing animate__delay-2s"><a href="/"><img src="/images/tkz-logo.png" /></a></div>
+    <div className="logo animate__animated animate__swing animate__delay-2s"><Link href="/"><img src="/images/tkz-logo.png" /></Link></div>
     
     <div className="menuIcon">
     <div className="iconOpenMenu"></div>
@@ -14,26 +15,26 @@ export default function Wrapper(props) {
     </div>
     
     <div className="sideMenu disNone767">
-    <a href="" title="Refresh" id="pageReload">
+    <Link href="" title="Refresh" id="pageReload">
     <span>
     <span>
    
-    </span></span></a>
+    </span></span></Link>
     
-    <a href="#" title="Full Screen" id="btnFullScreen">
+    <Link href="#" title="Full Screen" id="btnFullScreen">
     <span>
     <span>
-    </span></span></a>
+    </span></span></Link>
     
-    <a href="/about-us" title="About Us">
+    <Link href="/about-us" title="About Us">
     <span>
     <span>
-    </span></span></a>
+    </span></span></Link>
     
-    <a href="/contact-us" title="Contact Us">
+    <Link href="/contact-us" title="Contact Us">
     <span>
     <span>
-    </span></span></a></div>
+    </span></span></Link></div>
     
     </header>
     {props.children}
